afe730601d1b6cfe4d287c599fd49fad068dbb07
junhui <daniel.zhujunhui@gmail.com>
2023-01-19 12:59:15 +0800

    define interfaces

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+pub struct KvStore;
+
+impl KvStore {
+    pub fn new() -> Self {
+        unimplemented!();
+    }
+
+
+    pub fn set(&self, key: String, value: String) {
+        unimplemented!();
+    }
+
+    pub fn remove(&self, key: String) {
+        unimplemented!();
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        unimplemented!();
+    }
+}
+
