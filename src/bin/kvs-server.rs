dbe797a7447828faada18c1c4f3de77ca49255c0
junhui <daniel.zhujunhui@gmail.com>
2023-01-23 23:03:46 +0800

    log

--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -1,7 +1,19 @@
+#[macro_use]
+extern crate log;
+
 use clap::Parser;
 use kvs::ServerCli;
 
+// get the version from Cargo.toml
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 fn main() {
+    env_logger::init();
+    // let cli = ServerCli::parse();
+    info!("The version of the lib: {}", VERSION);
+
     let cli = ServerCli::parse();
-    println!("Hello, world!");
+    let engine = cli.engine;
+    let addr = cli.addr;
+    info!("The addr of the server: {}", &addr);
+    info!("The engine of the server: {}", &engine);
 }
